2591cabca2cfcb8a7f1ec4993e9d7d14b1d7c8f1
Craig Weeks <craigweeks99@gmail.com>
2018-01-03 23:34:01 -0800

    i did something

---
+++ Firebase test/www/scripts/index.js
@@ -0,0 +1,66 @@
+﻿(function () {
+    "use strict";
+
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        //var parentElement = document.getElementById('deviceready');
+        //var listeningElement = parentElement.querySelector('.listening');
+        // var receivedElement = parentElement.querySelector('.received');
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
+
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyD1ErUGsqUboJTgJMo2wNIVABkBgW_bs7Y",
+            authDomain: "key-club-5dd8a.firebaseapp.com",
+            databaseURL: "https://key-club-5dd8a.firebaseio.com",
+            projectId: "key-club-5dd8a",
+            storageBucket: "key-club-5dd8a.appspot.com",
+            messagingSenderId: "367057032737"
+        };
+        firebase.initializeApp(config);
+
+
+        var addButton = document.getElementById("addToDb");
+        var readButton = document.getElementById("readDb");
+        var data = {
+            Text:"Hello World!"
+        };
+
+        addButton.addEventListener("click", function () {
+
+            firebase.database().ref().child('Data').push(data);
+            console.log("data Pushed");
+        });
+
+        readButton.addEventListener("click", function () {
+
+            firebase.database().ref().child('Data').on("child_added", snap => {
+
+                var output = snap.val().Text;
+                document.getElementById("txtArea").innerHTML = output;
+
+            });
+            console.log("data read");
+
+        });
+
+    };
+
+ 
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+} )();
